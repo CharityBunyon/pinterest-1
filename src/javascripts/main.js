195c1d858b87b22fe085560d7e3d88e4431dfa95
Ashley Claiborne <languageisaliquid@gmail.com>
2019-11-01 22:50:02 -0500

    toggle display on auth change

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -3,10 +3,12 @@ import '../styles/main.scss';
 import firebase from 'firebase/app';
 import apiKeys from './helpers/apiKeys.json';
 import auth from './components/auth/auth';
+import authData from './helpers/data/authData';
 
 const init = () => {
   firebase.initializeApp(apiKeys.firebaseConfig);
   auth.printLoginBtn();
+  authData.checkLoginStatus();
 };
 
 init();
